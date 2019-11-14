90e87e807b6aebd761233a10ed33128dae700897
Alfikri <belintex4@gmail.com>
2019-11-14 22:03:55 +0700

    budi

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <?php 
 
 echo "==xxNamaxx==";
-echo "topan";
-echo "Alfikri";
-echo "topan";
-echo "Alfikri";
-echo "By";
-echo "ipaannn";
-echo "toapn";
-echo "topaaaan";
-echo "topaaaan";
+echo "Budi";
+
 ?>
