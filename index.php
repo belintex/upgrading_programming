c3f235acb6c1fc8049400c9d63c53973313d5a32
muhammadtopan <taufanomt@gmail.com>
2019-11-14 22:00:16 +0700

    sddsd

--- index.php
+++ index.php
@@ -7,10 +7,7 @@ echo "topan";
 echo "Alfikri";
 echo "By";
 echo "ipaannn";
-<<<<<<< HEAD
 echo "toapn";
 echo "topaaaan";
-=======
-
->>>>>>> 1b1298d66eb675b26bbf4159777330fb696f0d6b
+echo "topaaaan";
 ?>
