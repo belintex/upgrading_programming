6992549a8a4849d7458462cbac16bf923a1ec5af
Alfikri <belintex4@gmail.com>
2019-11-14 22:57:12 +0700

    ===Lagi===

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
 <?php 
-// kalah terakhir pushup 30
-// kalah 2 terakhir pushup 20
-// kalah 3 terakhir pushup 10
-
-echo "xx==Mulai==xx";
-echo "ini tambahan";
-
+echo "Buat Nama Anda dibawah";
 ?>
--- soal.php
+++
@@ -1,7 +0,0 @@
-<?php 
-// kalah terakhir pushup 30
-// kalah 2 terakhir pushup 20
-// kalah 3 terakhir pushup 10
-echo "==Start==";
-
-?>
