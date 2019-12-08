b96d192f433c43d5aba4b8d9af31017ae126e55e
budbuuds <budiagungsantoso572@gmail.com>
2019-12-08 21:03:16 +0700

    budi selesai

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php 
+//tugas nya buat branch masing2
+//lalu pindah ke branch yang telah di buat
+//ambil data dari branch master ke branch masing2
+//tambahkan nama di codingan
+//lalu push di branch masing2 dan branch master
+//dah itu aja
+//harus selesai hari ini
 echo "xx==Mulai==xx";
-echo "Nama";
-echo "bahagia";
+echo "Mutiah Risky";
+echo "Fajar Maulana";
+echo "Inggi Cantik";
+echo "Syintia Adriani";
+echo "koor tercinta";
+echo "ganteng.com";
+echo "robi";
 ?>
